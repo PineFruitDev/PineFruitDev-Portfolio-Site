bd146262f3260ffcbc52d04e5c01189fe40ec5b4
PineFruit <141669954+PineFruitDev@users.noreply.github.com>
2025-10-01 17:24:43 -0400

    Image Update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -70,6 +70,7 @@ export const metadata: Metadata = {
   },
   icons: {
     icon: '/favicon.ico',
+    shortcut: '/favicon.ico',
     apple: '/apple-touch-icon.png',
   },
   manifest: '/manifest.json',
